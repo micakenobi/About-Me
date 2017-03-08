dc34b293cad65f6e16fe0ff2b5be1368b7f51877
micakenobi <beckyedithbrooker@gmail.com>
2017-03-07 19:37:38 -0800

    I have added 5 questions. The first question in regards to the user name. The next four questions are yes/no questions for the game. I also added console.log statements for each question to write to the console. Oh yeah, each question has a yes/no/other response to cover all bases.

--- app.js
+++ app.js
@@ -2,4 +2,54 @@
 
 function functionName(){
   alert('Time to Play!');
+
+  var question1 = 'What is your name?';
+  var answerUserName = prompt(question1);
+  console.log(question1 + ': ' + answerUserName);
+  var response1 = 'Well hello there, ' + answerUserName;
+  alert(response1);
+
+  var question2 = 'Do you also like to travel?';
+  var answerTravel = prompt(question2);
+  if (answerTravel.toLowerCase() === 'yes' || answerTravel.toLowerCase() === 'y'){
+    alert ('Let\'s plan a trip!');
+  } else if (answerTravel.toLowerCase() === 'no' || answerTravel.toLowerCase() === 'n') {
+    alert ('You are dull...');
+  } else {
+    alert('You didn\'t put a proper answer. Try "yes", "y", "no", or "n".');
+  }
+  console.log(question2 + ': ' + answerTravel);
+
+  var question3 = 'Do you like to eat dessert?';
+  var answerDessert = prompt(question3);
+  if (answerDessert.toLowerCase() === 'yes' || answerDessert.toLowerCase() === 'y'){
+    alert ('Ready for cake!');
+  } else if (answerDessert.toLowerCase() === 'no' || answerDessert.toLowerCase() === 'n') {
+    alert ('Live a little...');
+  } else {
+    alert('You didn\'t put a proper answer. Try "yes", "y", "no", or "n".');
+  }
+  console.log(question3 + ': ' + answerDessert);
+
+  var question4 = 'Do you like running?';
+  var answerRun = prompt(question4);
+  if (answerRun.toLowerCase() === 'yes' || answerRun.toLowerCase() === 'y'){
+    alert ('Grab your sneakers!');
+  } else if (answerRun.toLowerCase() === 'no' || answerRun.toLowerCase() === 'n') {
+    alert ('Shucks...');
+  } else {
+    alert('You didn\'t put a proper answer. Try "yes", "y", "no", or "n".');
+  }
+  console.log(question4 + ': ' + answerRun);
+
+  var question5 = 'Do you like puppies?';
+  var answerPup = prompt(question5);
+  if (answerPup.toLowerCase() === 'yes' || answerPup.toLowerCase() === 'y'){
+    alert ('Puppy time!');
+  } else if (answerPup.toLowerCase() === 'no' || answerPup.toLowerCase() === 'n') {
+    alert ('Don\'t tell me you like cats...');
+  } else {
+    alert('You didn\'t put a proper answer. Try "yes", "y", "no", or "n".');
+  }
+  console.log(question5 + ': ' + answerPup);
 }
